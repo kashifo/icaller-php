25a313e8bbf8827652c1f8b79bd6d12f100120b8
Kashif Anwaar <koderkashif@gmail.com>
2018-03-27 00:56:55 +0530

    Modified scanner to fetch interests of profiles

--- apis/scan_profiles.php
+++ apis/scan_profiles.php
@@ -7,7 +7,7 @@ if (isset ($_POST['interests'])) {
     $arraySize = sizeof ($interestArray);
 
     //SELECT * from users WHERE id IN ( SELECT userID FROM interests WHERE JavaScript OR Android IS NOT NULL );
-    
+
     $sql = "SELECT * from users WHERE id IN ( SELECT userID FROM interests WHERE ";
 
     for ($i = 0; $i < $arraySize; $i++) {
@@ -18,7 +18,7 @@ if (isset ($_POST['interests'])) {
 
     $sql .= " IS NOT NULL );";
     //echo $sql;
-    
+
     require_once '../include/DB_Connect.php';
     // connecting to database
     $db = new Db_Connect();
@@ -27,20 +27,52 @@ if (isset ($_POST['interests'])) {
     $result = mysqli_query ($conn, $sql);
 
     if (!empty ($result)) {
-    // check for empty result
-    if (mysqli_num_rows ($result) > 0) {
+        // check for empty result
+        if (mysqli_num_rows ($result) > 0) {
 
-        $count = mysqli_num_rows($result);
-        // success
-        $response["success"] = 1;
-        $response["message"] = "$count users found";
-        $response["users"] = array ();
+            $count = mysqli_num_rows ($result);
+            // success
+            $response["success"] = 1;
+            $response["message"] = "$count users found";
+            $response["users"] = array ();
 
-        while ($row = mysqli_fetch_assoc ($result)) {
-            array_push ($response["users"] , $row);
+            while ($row = mysqli_fetch_assoc ($result)) {
+                //array_push ($response["users"] , $row);
+
+                $user = array ();
+                $user = $row;
+
+                $ints = array ();
+                $result2 = mysqli_query ($conn, "SELECT * FROM interests WHERE userID = $row[id]");
+
+                if (!empty ($result2)) {
+                    // check for empty result
+                    if (mysqli_num_rows ($result2) > 0) {
+
+                        $row2 = mysqli_fetch_assoc ($result2);
+
+                        foreach ($row2 as $key => $value) {
+                            if ($value == 1) {
+                                array_push ($ints, $key);
+                            }
+                        }
+                    }
+                }
+
+                // interests node
+                $user["interests"] = $ints;
+                array_push ($response["users"], $user);
+            }
+
+            echo json_encode ($response);
+        } else {
+            // no product found
+            $response["success"] = 0;
+            $response["message"] = "No user found for your interests";
+
+            // echo no users JSON
+            echo json_encode ($response);
         }
-
-        echo json_encode ($response);
     } else {
         // no product found
         $response["success"] = 0;
@@ -49,14 +81,6 @@ if (isset ($_POST['interests'])) {
         // echo no users JSON
         echo json_encode ($response);
     }
-} else {
-    // no product found
-    $response["success"] = 0;
-    $response["message"] = "No user found for your interests";
-
-    // echo no users JSON
-    echo json_encode ($response);
-}
 } else {
     $response["error"] = TRUE;
     $response["error_msg"] = "Required parameters are missing!";
